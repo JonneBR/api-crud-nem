77fb515970afb724f7bd5e24e174c04239a288c3
unknown <joaomgoncalvesbr@gmail.com>
2022-04-20 16:44:02 -0300

    first commit after setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+//Forma de ler JSON
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+//rota inicial / endpoint
+app.get("/", (req, res) => {
+  //mostrar req
+
+  res.json({ message: "First route created! Congratulations!!!" });
+});
+
+//entregar uma porta
+app.listen();
